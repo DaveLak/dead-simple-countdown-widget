70e27f8d582c20bb86e833f3c0de6c162b59b4ba
David Lakin <dal.scny@gmail.com>
2018-03-20 01:16:24 -0400

    Fix spacing & comments.
    * Spaces are replaced with tabs
    * Comments now have proper punctuation
    * Arrays have trailing commas
    * Add missing semi-colon to End date text input's value

--- inc/countdown-widget.php
+++ inc/countdown-widget.php
@@ -18,16 +18,16 @@ if ( ! class_exists( 'Dead_Simple_CountDown_Widget' ) ) {
 		public function __construct() {
 			$widget_options = array(
 				'classname'   => 'dscw-countdown-widget',
-				'description' => __( '', 'dead_simple_countdown' )
+				'description' => __( '', 'dead_simple_countdown' ),
 			);
 			parent::__construct( 'dscw-countdown-widget', __( 'Dead Simple CountDown', 'dead_simple_countdown' ), $widget_options );
 		}
 
 
 		/**
-		 * Setting form
+		 * Settings form.
 		 *
-		 * @param array $instance Current instance of the widget
+		 * @param array $instance Current instance of the widget.
 		 */
 		public function form( $instance ) {
 
@@ -36,78 +36,78 @@ if ( ! class_exists( 'Dead_Simple_CountDown_Widget' ) ) {
 			$end_date_ms  = ! empty( $instance['end_date_ms'] ) ? $instance['end_date_ms'] : '';
 			$expired_text = ! empty( $instance['expired_text'] ) ? $instance['expired_text'] : '';
 
-			// If no theme is set default to "light"
-			$theme        = ! empty( $instance['theme'] ) ? $instance['theme'] : 'light';
+			// If no theme is set default to "light".
+			$theme = ! empty( $instance['theme'] ) ? $instance['theme'] : 'light';
 
 			ob_start();
 			?>
-            <p>
-                <label for="<?php echo $this->get_field_id( 'title_text' ); ?>">Title:</label>
-                <input type="text" id="<?php echo $this->get_field_id( 'title_text' ); ?>"
-                       name="<?php echo $this->get_field_name( 'title_text' ); ?>" class="widefat"
-                       value="<?php echo esc_attr( $title_text ); ?>"
-                />
-            </p>
-            <hr>
-            <p>
-                <label for="<?php echo $this->get_field_id( 'theme' ); ?>">Theme:</label>
-                <select name="<?php echo $this->get_field_name( 'theme' ); ?>"
-                        id="<?php echo $this->get_field_id( 'theme' ); ?>">
-                    <option value="light" <?php echo $theme === 'light' ? 'selected' : ''; ?>>Light</option>
-                    <option value="dark" <?php echo $theme === 'dark' ? 'selected' : ''; ?>>Dark</option>
-                    <option value="none"<?php echo $theme === 'none' ? 'selected' : ''; ?>>
-                        None
-                    </option>
-                </select>
-            </p>
-            <hr>
-            <p>
-                <label for="<?php echo $this->get_field_id( 'end_date' ); ?>">
-                    End Date:
-                    <input type="text" id="<?php echo $this->get_field_id( 'end_date' ); ?>"
-                           name="<?php echo $this->get_field_name( 'end_date' ); ?>"
-                           value="<?php echo $end_date ?>"
-                           onclick="jQuery(this).datepicker({
-                                        altField: '#<?php echo $this->get_field_id( 'end_date_ms' ); ?>',
-                                        altFormat: '@'
-                                    });
-                                   jQuery(this).datepicker('show');
-                                   "
-                    />
-                    <span style="display: block; font-size: 0.9em; margin-top: 4px; color: #656572;" >
-                        Date to count down to.
-                    </span>
-                </label>
-                <input type="hidden" id="<?php echo $this->get_field_id( 'end_date_ms' ); ?>"
-                       name="<?php echo $this->get_field_name( 'end_date_ms' ); ?>"
-                       value="<?php echo $end_date_ms ?>"
-                />
-            </p>
-            <hr>
-            <p>
-                <label for="<?php echo $this->get_field_id( 'expired_text' ); ?>">
-                    End Date Text:
-                    <input type="text" id="<?php echo $this->get_field_id( 'expired_text' ); ?>"
-                           name="<?php echo $this->get_field_name( 'expired_text' ); ?>"
-                           value="<?php echo $expired_text ?>"
-                    />
-                    <span style="display: block; font-size: 0.9em; margin-top: 4px; color: #656572;">
-                        Text displayed when countdown expires.
-                    </span>
-                </label>
-            </p>
+			<p>
+				<label for="<?php echo $this->get_field_id( 'title_text' ); ?>">Title:</label>
+				<input type="text" id="<?php echo $this->get_field_id( 'title_text' ); ?>"
+					   name="<?php echo $this->get_field_name( 'title_text' ); ?>" class="widefat"
+					   value="<?php echo esc_attr( $title_text ); ?>"
+				/>
+			</p>
+			<hr>
+			<p>
+				<label for="<?php echo $this->get_field_id( 'theme' ); ?>">Theme:</label>
+				<select name="<?php echo $this->get_field_name( 'theme' ); ?>"
+						id="<?php echo $this->get_field_id( 'theme' ); ?>">
+					<option value="light" <?php echo $theme === 'light' ? 'selected' : ''; ?>>Light</option>
+					<option value="dark" <?php echo $theme === 'dark' ? 'selected' : ''; ?>>Dark</option>
+					<option value="none"<?php echo $theme === 'none' ? 'selected' : ''; ?>>
+						None
+					</option>
+				</select>
+			</p>
+			<hr>
+			<p>
+				<label for="<?php echo $this->get_field_id( 'end_date' ); ?>">
+					End Date:
+					<input type="text" id="<?php echo $this->get_field_id( 'end_date' ); ?>"
+						   name="<?php echo $this->get_field_name( 'end_date' ); ?>"
+						   value="<?php echo $end_date; ?>"
+						   onclick="jQuery(this).datepicker({
+										altField: '#<?php echo $this->get_field_id( 'end_date_ms' ); ?>',
+										altFormat: '@'
+									});
+								   jQuery(this).datepicker('show');
+								   "
+					/>
+					<span style="display: block; font-size: 0.9em; margin-top: 4px; color: #656572;" >
+						Date to count down to.
+					</span>
+				</label>
+				<input type="hidden" id="<?php echo $this->get_field_id( 'end_date_ms' ); ?>"
+					   name="<?php echo $this->get_field_name( 'end_date_ms' ); ?>"
+					   value="<?php echo $end_date_ms; ?>"
+				/>
+			</p>
+			<hr>
+			<p>
+				<label for="<?php echo $this->get_field_id( 'expired_text' ); ?>">
+					End Date Text:
+					<input type="text" id="<?php echo $this->get_field_id( 'expired_text' ); ?>"
+						   name="<?php echo $this->get_field_name( 'expired_text' ); ?>"
+						   value="<?php echo $expired_text; ?>"
+					/>
+					<span style="display: block; font-size: 0.9em; margin-top: 4px; color: #656572;">
+						Text displayed when countdown expires.
+					</span>
+				</label>
+			</p>
 
 			<?php
 			echo ob_get_clean();
 		}
 
 		/**
-		 * Update logic for this instance
+		 * Update logic for this instance.
 		 *
-		 * @param array $new_instance Widget settings being saved now
-		 * @param array $old_instance Last saved settings for this widget instance
+		 * @param array $new_instance Widget settings being saved now.
+		 * @param array $old_instance Last saved settings for this widget instance.
 		 *
-		 * @return array $instance Updated widget
+		 * @return array $instance Updated widget.
 		 */
 		public function update( $new_instance, $old_instance ) {
 			$instance = $old_instance;
@@ -122,7 +122,6 @@ if ( ! class_exists( 'Dead_Simple_CountDown_Widget' ) ) {
 
 			$instance['expired_text'] = sanitize_text_field( $new_instance['expired_text'] );
 
-
 			return $instance;
 		}
 
@@ -134,12 +133,12 @@ if ( ! class_exists( 'Dead_Simple_CountDown_Widget' ) ) {
 			wp_enqueue_script( 'dead-simple-countdown-widget-js' );
 			wp_enqueue_style( 'dead-simple-countdown-widget-styles' );
 
-			// UNIX timestamp in milliseconds of the date the countdown is set to expire
-			$endDate_ms  = $instance['end_date_ms'];
-			// Text to display when countdown expires
+			// UNIX timestamp in milliseconds of the date the countdown is set to expire.
+			$endDate_ms = $instance['end_date_ms'];
+			// Text to display when countdown expires.
 			$expiredText = $instance['expired_text'];
 
-			// Check if we are using a theme and set the CSS class accordingly
+			// Check if we are using a theme and set the CSS class accordingly.
 			switch ( $instance['theme'] ) {
 				case 'light':
 					$themeClass = 'dscw-countdown-theme-light';
@@ -152,24 +151,24 @@ if ( ! class_exists( 'Dead_Simple_CountDown_Widget' ) ) {
 					break;
 			}
 
-			// If a theme is set then add an class for inner wrap
+			// If a theme is set then add an class for inner wrap.
 			$innerThemeClass = $themeClass ? 'dscw-countdown-theme-inner' : '';
 
 			$content = '';
-			// Primary container element. This will carry our options to the front-end via data attributes
+			// Primary container element. This will carry our options to the front-end via data attributes.
 			$content .= '<div 
 		                class="dscw-countdown-instance ' . $themeClass . '" 
 		                data-instance="' . $this->id . '" 
 		                data-end-date="' . $endDate_ms . '" 
 		                data-expired-text="' . $expiredText . '"
 		             >';
-			// Inner theme wrapper. This gives us another selector to style with
+			// Inner theme wrapper. This gives us another selector to style with.
 			$content .= '<div class="' . $innerThemeClass . '">';
 			$content .= '<h3>' . $instance['title_text'] . '</h3>';
-			// Mount point to build the countdown in
+			// Mount point to build the countdown in.
 			$content .= '<div id="timer-mount-' . $this->id . '"></div>';
-			$content .= '</div>'; // Close inner theme wrapper
-			$content .= '</div>'; // Close primary container
+			$content .= '</div>'; // Close inner theme wrapper.
+			$content .= '</div>'; // Close primary container.
 
 			$output = $args['before_widget'] . $content . $args['after_widget'];
 			echo $output;
