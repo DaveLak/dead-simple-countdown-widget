c5e78eb748bf907db4a3cca0a1e7be3d2bb0133f
David Lakin <dal.scny@gmail.com>
2018-03-21 16:48:41 -0400

    Return CountDownTimer as a closure with methods on prototype.
    * Internal time bases moved to prototype.
    * Instantiation now only initializes the class.
      - Initialize properties in constructor
      - Add `start()` method to draw and start the countdown.

--- assets/front-end/js/countdown-timer.js
+++ assets/front-end/js/countdown-timer.js
@@ -17,59 +17,23 @@
             // UNIX timestamp in milliseconds of the date the countdown is set to expire
             this.end = settings.endDate;
 
-            // Remaining time
+            // Initialize remaining time as undefined
             this.days = undefined;
             this.hours = undefined;
             this.minutes = undefined;
             this.seconds = undefined;
 
-            // Draws the box be put the timer in and mounts it to this.$mountPoint
-            this.render = function () {
-
-                // ID strings for created HTML elements
-                var timerID = this.instanceID + '-countdown';
-                var daysID = this.instanceID + '-days';
-                var hoursID = this.instanceID + '-hours';
-                var minutesID = this.instanceID + '-minutes';
-                var secondsID = this.instanceID + '-seconds';
-
-                // Create and mount the container
-                this.$countDownBox = $('<div/>', {
-                    id: timerID,
-                    class: 'dscw-countdown-timer-box-container'
-                }).appendTo(this.$mountPoint);
-
-                this.$countDownBox.html('' +
-                    '<ul class="dscw-countdown-timer-box">' +
-                    '    <li class="dscw-countdown-timer-box-items dscw-countdown-days">' +
-                    '       <div id="' + daysID + '" class="dscw-countdown-number">00</div>' +
-                    '       <div class="dscw-countdown-label">Days</div>' +
-                    '   </li>' +
-                    '    <li class="dscw-countdown-timer-box-items dscw-countdown-hours">' +
-                    '       <div id="' + hoursID + '" class="dscw-countdown-number">00</div>' +
-                    '       <div class=" dscw-countdown-label">Hours</div>' +
-                    '   </li>' +
-                    '    <li class="dscw-countdown-timer-box-items dscw-countdown-minutes">' +
-                    '       <div id="' + minutesID + '" class="dscw-countdown-number">00</div>' +
-                    '       <div class=" dscw-countdown-label">Minutes</div>' +
-                    '   </li>' +
-                    '    <li class="dscw-countdown-timer-box-items dscw-countdown-seconds">' +
-                    '       <div id="' + secondsID + '" class="dscw-countdown-number">00</div>' +
-                    '       <div class=" dscw-countdown-label">Seconds</div>' +
-                    '   </li>' +
-                    '</ul>'
-                );
-
-                // Assign jQuery objects representing each element holding a number to class properties
-                this.$numberDays = $('#' + daysID);
-                this.$numberHours = $('#' + hoursID);
-                this.$numberMinutes = $('#' + minutesID);
-                this.$numberSeconds = $('#' + secondsID);
-            };
+            // Initialize objects representing each element holding a number
+            this.$numberDays = undefined;
+            this.$numberHours = undefined;
+            this.$numberMinutes = undefined;
+            this.$numberSeconds = undefined;
 
         }
 
-        /* Properties */
+        /************************
+         * Prototype Properties *
+         ************************/
 
         // Time Bases in Milliseconds
         CountDownTimer.prototype._second = 1000;
@@ -77,15 +41,16 @@
         CountDownTimer.prototype._hour = 3600000;
         CountDownTimer.prototype._day = 86400000;
 
-        /* Methods */
+        /***********************
+         *  Prototype Methods  *
+         ***********************/
 
-        CountDownTimer.prototype.init = function () {
+        CountDownTimer.prototype.start = function () {
             // Reference to calling class
             var self = this;
 
             // Draw the timer
             this.render();
-            this.calculateRemaining();
 
             this.timer = setInterval(function () {
                 // Run the actual countdown until it expires
@@ -93,32 +58,24 @@
             }, 1000);
         };
 
-        CountDownTimer.prototype.render = function () {};
-
-        // Calculate time left until expiration
+        // Calculate and update reaming days, hours, minutes, seconds until expiration
         CountDownTimer.prototype.calculateRemaining = function () {
-            // Time right now
-            var now = new Date();
             // Time left until countdown expires
-            var timeRemaining = this.end - now;
+            var timeRemaining = this.end - new Date();
 
-            if (timeRemaining > 0) {
-                this.days = Math.floor(timeRemaining / this._day);
-                this.hours = Math.floor((timeRemaining % this._day) / this._hour);
-                this.minutes = Math.floor((timeRemaining % this._hour) / this._minute);
-                this.seconds = Math.floor((timeRemaining % this._minute) / this._second);
-                // There is still time left
-                return true;
-            }
-            // Time expired
-            return false;
+            this.days = Math.floor(timeRemaining / this._day);
+            this.hours = Math.floor((timeRemaining % this._day) / this._hour);
+            this.minutes = Math.floor((timeRemaining % this._hour) / this._minute);
+            this.seconds = Math.floor((timeRemaining % this._minute) / this._second);
+
+            return timeRemaining;
         };
 
         // Recalculates time left in countdown, updating numbers as needed
         CountDownTimer.prototype.showRemainingTime = function () {
 
             // Check if there is still time left before the countdown expires
-            if (this.calculateRemaining()) {
+            if (this.calculateRemaining() > 0) {
                 // There is still time left
                 // Update the displayed numbers
                 this.$numberDays.text(this.days);
@@ -135,6 +92,51 @@
             }
         };
 
+        // Draws the box be put the timer in and mounts it to this.$mountPoint
+        CountDownTimer.prototype.render = function () {
+
+            // ID strings for created HTML elements
+            var timerID = this.instanceID + '-countdown';
+            var daysID = this.instanceID + '-days';
+            var hoursID = this.instanceID + '-hours';
+            var minutesID = this.instanceID + '-minutes';
+            var secondsID = this.instanceID + '-seconds';
+
+            // Create and mount the container
+            this.$countDownBox = $('<div/>', {
+                id: timerID,
+                class: 'dscw-countdown-timer-box-container'
+            }).appendTo(this.$mountPoint);
+
+            this.$countDownBox.html('' +
+                '<ul class="dscw-countdown-timer-box">' +
+                '    <li class="dscw-countdown-timer-box-items dscw-countdown-days">' +
+                '       <div id="' + daysID + '" class="dscw-countdown-number">00</div>' +
+                '       <div class="dscw-countdown-label">Days</div>' +
+                '   </li>' +
+                '    <li class="dscw-countdown-timer-box-items dscw-countdown-hours">' +
+                '       <div id="' + hoursID + '" class="dscw-countdown-number">00</div>' +
+                '       <div class=" dscw-countdown-label">Hours</div>' +
+                '   </li>' +
+                '    <li class="dscw-countdown-timer-box-items dscw-countdown-minutes">' +
+                '       <div id="' + minutesID + '" class="dscw-countdown-number">00</div>' +
+                '       <div class=" dscw-countdown-label">Minutes</div>' +
+                '   </li>' +
+                '    <li class="dscw-countdown-timer-box-items dscw-countdown-seconds">' +
+                '       <div id="' + secondsID + '" class="dscw-countdown-number">00</div>' +
+                '       <div class=" dscw-countdown-label">Seconds</div>' +
+                '   </li>' +
+                '</ul>'
+            );
+
+            // Assign jQuery objects representing each element holding a number to class properties
+            this.$numberDays = $('#' + daysID);
+            this.$numberHours = $('#' + hoursID);
+            this.$numberMinutes = $('#' + minutesID);
+            this.$numberSeconds = $('#' + secondsID);
+        };
+
+        // Return CountDownTimer class
         return CountDownTimer;
     })();
 
@@ -151,8 +153,7 @@
                 expiredText: $(this).data("expired-text"),
                 $mountPoint: $('#timer-mount-' + instanceID)
             };
-            var widget = new CountDownTimer(settings);
-            widget.init();
+            new CountDownTimer(settings).start();
         })
     });
 })(jQuery);
